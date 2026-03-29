563af4b60f8994aa2a9c21d5c7988ebb060c1740
Datt <65765665+dat02052006@users.noreply.github.com>
2026-03-29 10:05:33 +0700

    Remove generated ModelSim work files
    Clean up the simulation 'work' library by deleting generated ModelSim/Model Technology artifacts and temporary files. Removes .dat, .dbs, .vhd, .prw, .psm and temp entries for modules (emitter, hamming_decoder, oversampling_filer, receiver, rx_fsm) as well as work/_info and work/_vmake to reset the work directory before a fresh rebuild.

--- work/@emitter/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity Emitter is
-    port(
-        d               : in     vl_logic_vector(7 downto 0);
-        encoded_data    : out    vl_logic_vector(12 downto 0)
-    );
-end Emitter;
